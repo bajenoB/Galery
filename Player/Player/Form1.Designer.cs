2b1765d0836304b38d40f340307546649ae75fed
bajenob <jonjill1488@gmail.com>
2021-07-04 21:34:12 +0300

    imagelist gallery

--- Player/Player/Form1.Designer.cs
+++ Player/Player/Form1.Designer.cs
@@ -44,7 +44,7 @@ namespace Player
             this.button2 = new Button();
             this.button3 = new Button();
             this.button4 = new Button();
-            this.pictureBox1 = new PictureBox();
+           
             this.imageList1 = new ImageList(this.components);
 
             this.panel1 = new Panel();
@@ -59,25 +59,24 @@ namespace Player
             this.button1.Location = new Point(96, 128);
             this.button1.Size = new Size(104, 23);
             this.button1.Text = "Show Next Image";
-            this.button1.Click += Button1_Click;
+            this.button1.Click += button1_Click;
 
             this.button2.Location = new Point(208, 128);
             this.button2.Size = new Size(104, 23);
             this.button2.Text = "Remove Image";
-            this.button2.Click += Button2_Click; 
+            this.button2.Click += button2_Click; 
 
             this.button3.Location = new Point(320, 128);
             this.button3.Text = "Clear List";
-            this.button3.Click += Button3_Click;
+            this.button3.Click += button3_Click;
 
             this.button4.Location = new Point(16, 128);
             this.button4.Text = "Open Image";
-            this.button4.Click += Button4_Click;
+            this.button4.Click += button4_Click;
 
-            this.pictureBox1.Location = new Point(328, 232);
-            this.pictureBox1.Size = new Size(336, 192);
+            
 
-            this.imageList1.ImageSize = new Size(16, 16);
+            this.imageList1.ImageSize = new Size(30, 30);
             this.imageList1.TransparentColor = Color.Transparent;
 
             this.panel1.Location = new Point(8, 240);
@@ -90,7 +89,7 @@ namespace Player
             
             this.Controls.Add(this.label5);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.pictureBox1);
+            
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -100,24 +99,74 @@ namespace Player
             this.ResumeLayout(false);
         }
 
-        private void Button4_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, System.EventArgs e)
         {
-            throw new NotImplementedException();
+            if (imageList1.Images.Empty != true)
+            {
+                if (imageList1.Images.Count - 1 > currentImage)
+                {
+                    currentImage++;
+                }
+                else
+                {
+                    currentImage = 0;
+                }
+                panel1.Refresh();
+
+                
+                imageList1.Draw(myGraphics, 10, 10, currentImage);
+
+                
+                
+                label3.Text = "Current image is " + currentImage;
+                listBox1.SelectedIndex = currentImage;
+                label5.Text = "Image is " + listBox1.Text;
+            }
         }
 
-        private void Button3_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, System.EventArgs e)
         {
-            throw new NotImplementedException();
+            imageList1.Images.RemoveAt(listBox1.SelectedIndex);
+            listBox1.Items.Remove(listBox1.SelectedItem);
         }
 
-        private void Button2_Click(object sender, EventArgs e)
+       
+        private void button3_Click(object sender, System.EventArgs e)
         {
-            throw new NotImplementedException();
+            imageList1.Images.Clear();
+            listBox1.Items.Clear();
         }
 
-        private void Button1_Click(object sender, EventArgs e)
+       
+        private void button4_Click(object sender, System.EventArgs e)
         {
-            throw new NotImplementedException();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            openFileDialog1.Multiselect = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (openFileDialog1.FileNames != null)
+                {
+                    for (int i = 0; i < openFileDialog1.FileNames.Length; i++)
+                    {
+                        addImage(openFileDialog1.FileNames[i]);
+                    }
+                }
+                else
+                {
+                    addImage(openFileDialog1.FileName);
+                }
+            }
+        }
+
+        private void addImage(string imageToLoad)
+        {
+            if (imageToLoad != "")
+            {
+                imageList1.Images.Add(Image.FromFile(imageToLoad));
+                listBox1.BeginUpdate();
+                listBox1.Items.Add(imageToLoad);
+                listBox1.EndUpdate();
+            }
         }
 
         ListBox listBox1;
@@ -126,7 +175,7 @@ namespace Player
         Button button2;
         Button button3;
         Button button4;
-        PictureBox pictureBox1;
+      
         ImageList imageList1;
         OpenFileDialog openFileDialog1;
         protected Graphics myGraphics;
